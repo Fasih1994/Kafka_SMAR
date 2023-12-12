06b9f28b0f69d38edd11d293c2374d756c75e23d
Fasih Uddin <fasihuddin1994@gmail.com>
2023-12-12 18:19:00 +0500

    Kafka integration done

--- purg_volumes.sh
+++ purg_volumes.sh
@@ -1,5 +1,2 @@
-sudo rm -r zookeeper/data
-sudo rm -r zookeeper/logs
-sudo rm -r kafka_1/data
-sudo rm -r kafka_2/data
-sudo rm -r kafka_3/data
+sudo rm -r cluster/data
+sudo rm -r cluster/logs
--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #/bin/zsh
-echo "Creating network k_cluster_net"
-docker network create -d bridge k_cluster_net
-echo "Starting zookeeper"
-docker compose -f $(pwd)/zookeeper/docker_compose.yml up -d
-sleep 3
-echo "Starting Kafka brokers 1, 2, 3"
-docker compose -f $(pwd)/kafka_1/docker_compose.yml up -d
-docker compose -f $(pwd)/kafka_2/docker_compose.yml up -d
-docker compose -f $(pwd)/kafka_3/docker_compose.yml up -d
-sleep 3
-echo "Starting Schema Registry"
-docker compose -f $(pwd)/schema_registry/docker_compose.yml up -d
-sleep 3
-echo "Starting Kafka connect"
-docker compose -f $(pwd)/k_connect/docker_compose.yml up -d
-sleep 3
+# echo "Creating network k_cluster_net"
+# docker network create -d bridge k_cluster_net
+echo "Starting kafka_cluster"
+docker compose -f $(pwd)/cluster/docker_compose.yml up -d
+# sleep 3
+# echo "Starting Kafka brokers 1, 2, 3"
+# docker compose -f $(pwd)/kafka_1/docker_compose.yml up -d
+# docker compose -f $(pwd)/kafka_2/docker_compose.yml up -d
+# docker compose -f $(pwd)/kafka_3/docker_compose.yml up -d
+# sleep 3
+# echo "Starting Schema Registry"
+# docker compose -f $(pwd)/schema_registry/docker_compose.yml up -d
+# # sleep 3
+# echo "Starting Kafka connect"
+# docker compose -f $(pwd)/k_connect/docker_compose.yml up -d
+# sleep 3
 echo "Done!"
