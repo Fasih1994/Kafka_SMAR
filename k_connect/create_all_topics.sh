5d359158be0f8cd8efb5417c8e81daadaa6a4aa9
Fasih Uddin <fasihuddin1994@gmail.com>
2024-01-09 15:23:17 +0500

    added create and delete topic scripts

--- k_connect/create_all_topics.sh
+++ k_connect/create_all_topics.sh
@@ -2,8 +2,9 @@
 
 task='create'
 platforms='tiktok linkedin instagram facebook twitter'
+working_dir=$(pwd)
 
 for platform in $platforms; do
-    /home/fasih/k_cluster_smar/venv/bin/python \
+    "${working_dir}/venv/bin/python" \
         scripts/admin/${task}_topic.py -t "${platform}_posts,${platform}_post_tasks,${platform}_post_task_finished,${platform}_comments,${platform}_comment_tasks,${platform}_comment_tasks_finished"
 done
